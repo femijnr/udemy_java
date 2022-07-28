8fe38624f6140cbf7bc86ce05d438552ae8fb847
“femijnr” <adefemiisaaco@gmail.com>
2022-07-28 09:35:43 +0200

    added eat method

--- class_project/src/Business/KehindeTest.java
+++ class_project/src/Business/KehindeTest.java
@@ -1,2 +1,12 @@
-package Business;public class KehindeTest {
+package Business;
+
+public class KehindeTest {
+    private String _name;
+    public KehindeTest(String name){
+
+        _name = name;
+    }
+    public void eat(String name){
+        System.out.println(name);
+    }
 }
