a9a2b8e8e10af6527e3c90d6ac8c8fed5152fb0c
“femijnr” <adefemiisaaco@gmail.com>
2022-07-28 09:13:52 +0200

    test

---
+++ class_project/src/NewsAgency.java
@@ -0,0 +1,5 @@
+import Business.Company;
+
+public class NewsAgency {
+    Company company;
+}
---
+++ start_project/src/car.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Constructor;
+
+public class car {
+    private string model;
+    private string year;
+    private strimg colour;
+    private string engine;
+    private string  doors;
+    private string address
+
+
+    Constructor
+
+
+}
