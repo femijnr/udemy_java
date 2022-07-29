226338ce6d0ec186de77f306c08067ec3dbf96df
“femijnr” <adefemiisaaco@gmail.com>
2022-07-29 10:17:31 +0200

    new

---
+++ class_project/src/ArrayDemo.java
@@ -0,0 +1,16 @@
+import java.awt.datatransfer.StringSelection;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+System.out.println(args.length);
+
+int[] numbx =  {10, 20, 30, 40, 50, 60, 70};
+        Double[] numbr = {10.3, 10.4, 20.65, 60.56, 90.72};
+
+        System.out.println(numbx.length);
+        System.out.println(numbr.length);
+
+        System.out.println(numbx[3]);
+        System.out.println(numbr[4]);
+    }
+}
