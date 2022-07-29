76b5338edc593acc33f527452facd6c493c701f3
“femijnr” <adefemiisaaco@gmail.com>
2022-07-29 21:47:48 +0200

    new

--- class_project/src/ArrayDemo.java
+++ class_project/src/ArrayDemo.java
@@ -2,15 +2,15 @@ import java.awt.datatransfer.StringSelection;
 
 public class ArrayDemo {
     public static void main(String[] args) {
-System.out.println(args.length);
+    System.out.println(args.length);
 
-int[] numbx =  {10, 20, 30, 40, 50, 60, 70};
-        Double[] numbr = {10.3, 10.4, 20.65, 60.56, 90.72};
+    int[] numbx =  {10, 20, 30, 40, 50, 60, 70};
+    Double[] numbr = {10.3, 10.4, 20.65, 60.56, 90.72};
 
-        System.out.println(numbx.length);
-        System.out.println(numbr.length);
+    System.out.println(numbx.length);
+    System.out.println(numbr.length);
 
-        System.out.println(numbx[3]);
-        System.out.println(numbr[4]);
+    System.out.println(numbx[3]);
+    System.out.println(numbr[4]);
     }
 }
---
+++ class_project/src/method/project.java
@@ -0,0 +1,19 @@
+package method;
+
+public class project {
+
+    public static void main(String[] args) {
+        multiply(48, 23, 3);
+        welcome();
+    }
+public static void multiply(int a, int b,int c){
+    System.out.println(a * b);
+    }
+
+    public static void welcome(){
+        System.out.println("you are very welcome to jet ski");
+
+
+    }
+
+}
